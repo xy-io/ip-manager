cbff861e54a069d7553fa960bda2ac8aaac6e5d6
xy-io <jay@project-xy.com>
2026-08-31 18:57:10 +0100

    Delete ios/IPManagerNative directory

--- ios/IPManagerNative/IPManagerNative/Views/IPListView.swift
+++
@@ -1,9 +0,0 @@
-// IPListView.swift
-// This file is intentionally minimal.
-// The list is rendered by SidebarView inside RootView.
-// Kept for Xcode project compatibility.
-import SwiftUI
-
-struct IPListView: View {
-    var body: some View { EmptyView() }
-}
